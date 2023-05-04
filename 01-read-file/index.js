b4387c96fe7327252a49afa187020c2148d355ba
Mialiokhin <a.mialiokhin@gmail.com>
2023-05-04 23:02:27 +0300

    fix: error out

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 const {stdout} = require('process');
-const {stderr} = require('process');
 const fileStream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
 fileStream.on('data', data => stdout.write(data));
-fileStream.on('error', err => stderr.write(err));
+fileStream.on('error', error => stdout.write(error.toString()));
