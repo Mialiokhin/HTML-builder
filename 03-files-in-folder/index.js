d6a9b03736205c2bd14f2d88d4eaed0e3cc493ad
Mialiokhin <a.mialiokhin@gmail.com>
2023-05-06 12:31:25 +0300

    feat: add solution to 03-files-in-folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const fsPromises = fs.promises;
+fsPromises.readdir(path.join(__dirname, 'secret-folder'), {
+  withFileTypes: true
+}).then(elements => {
+  elements.forEach(element => {
+    if (!element.isDirectory()) {
+      const filePath = path.join(__dirname, 'secret-folder', element.name);
+      const fileName = path.basename(filePath);
+      const fileExt = path.extname(filePath);
+      fsPromises.stat(filePath)
+        .then(res => {
+          console.log(`${fileName.replace(fileExt, '')} - ${fileExt.replace('.', '')} - ${Number(res.size / 2000)}kb`);
+        });
+    }
+  });
+});
